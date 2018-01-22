f97525a7db219540bed03cdbd5e900d83f1a2680
amc2375 <amc2375@barnard.edu>
2018-01-22 18:58:38 -0500

    first attempt

--- sketch.js
+++ sketch.js
@@ -3,6 +3,13 @@ function setup() {
 }
 
 function draw() {
+	background(230, 230, 250);
+	fill(255, 240, 245);
+	triangle(24, 15, 35, 50, 75, 15);
+	textSize(36);
+	textFont('Georgia');
+	fill(105, 105, 105);
+	text('I hope you have a great day!', 50, 15)
   if (mouseIsPressed) {
     fill(0);
   } else {
