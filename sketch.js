180fad1e733b6f439c508277eeb54bb232270060
amc2375 <amc2375@barnard.edu>
2018-01-22 19:03:41 -0500

    second attempt

--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 650);
 }
 
 function draw() {
 	background(230, 230, 250);
-	fill(255, 240, 245);
-	triangle(24, 15, 35, 50, 75, 15);
-	textSize(36);
+	fill(152, 251, 152);
+	triangle(300, 100, 400, 50, 300, 100);
+	textSize(54);
 	textFont('Georgia');
 	fill(105, 105, 105);
-	text('I hope you have a great day!', 50, 15)
+	text('I hope you have a great day!', 400, 400)
   if (mouseIsPressed) {
     fill(0);
   } else {
