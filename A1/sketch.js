74d299936ce66f0e5d4306de566a3dc1dd17f86e
amc2375 <amc2375@barnard.edu>
2018-01-30 23:43:04 -0500

    tester 4

--- A1/sketch.js
+++ A1/sketch.js
@@ -3,15 +3,20 @@ function setup() {
   noStroke()
 }
 
+//188 was the G value for background; map(second(), 0, 59, 0, 255)
+// text size: map(minute(), 0, 59, 12, 100)
 function draw() {
 	background(143, 188, 143);
 	fill(152, 251, 152);
-	triangle(45, 45, 45, 360, 200, 360);
+	push();
+		rotate(map(second(), 0, 23, 0, 2*PI))
+		triangle(45, 45, 45, 360, 200, 360);
+	pop();
 	arc(479, 300, 280, 280, PI, TWO_PI)
 	textSize(54);
 	textFont('Georgia');
 	fill(47, 79, 79);
-	text('I hope you have a great day!', 200, 350)
+	text('I hope you have a great day!!!', 200, 350)
   if (mouseIsPressed) {
     fill(47, 79, 79);
   } else {
