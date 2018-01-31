8b773f47778f1bfae08afa5a86911855cb3b9997
amc2375 <amc2375@barnard.edu>
2018-01-31 13:41:22 -0500

    rotation update 2

--- A1/sketch.js
+++ A1/sketch.js
@@ -8,14 +8,15 @@ function setup() {
 function draw() {
 	background(143, map(second(), 0, 59, 0, 255), 143);
 	fill(152, 251, 152);
-	push();
-		rotate(map(hour(), 0, 23, 0, -PI))
-		triangle(45, 45, 45, 360, 200, 360);
-	pop();
+	//push();
+		//rotate(map(hour(), 0, 23, 0, -PI))
+	triangle(45, 45, 45, 360, 200, 360);
+	//pop();
 	arc(479, 300, 280, 280, PI, TWO_PI)
 	textSize(map(minute(), 0, 59, 12, 100));
 	textFont('Georgia');
 	fill(47, 79, 79);
+	rotate(map(hour(), 0, 23, 0, -PI))
 	text('I hope you have a great day!!!', 200, 350)
   if (mouseIsPressed) {
     fill(47, 79, 79);
