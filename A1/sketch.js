26e7c612a168905bdd72ff8e39295e1ad7916be3
amc2375 <amc2375@barnard.edu>
2018-01-30 23:50:03 -0500

    fourth try

--- A1/sketch.js
+++ A1/sketch.js
@@ -6,14 +6,14 @@ function setup() {
 //188 was the G value for background; map(second(), 0, 59, 0, 255)
 // text size: map(minute(), 0, 59, 12, 100)
 function draw() {
-	background(143, 188, 143);
+	background(143, map(second(), 0, 59, 0, 255), 143);
 	fill(152, 251, 152);
 	push();
-		rotate(map(second(), 0, 23, 0, 2*PI))
+		rotate(map(hour(), 0, 23, 0, 2*PI))
 		triangle(45, 45, 45, 360, 200, 360);
 	pop();
 	arc(479, 300, 280, 280, PI, TWO_PI)
-	textSize(54);
+	textSize(map(minute(), 0, 59, 12, 100));
 	textFont('Georgia');
 	fill(47, 79, 79);
 	text('I hope you have a great day!!!', 200, 350)
