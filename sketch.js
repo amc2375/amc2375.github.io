0a1d5afa05aea45594436668cfa275bc63ea6175
amc2375 <amc2375@barnard.edu>
2018-01-30 23:07:01 -0500

    tester 3

--- sketch.js
+++ sketch.js
@@ -1,17 +1,13 @@
+
 function setup() {
   createCanvas(1440, 800);
   noStroke()
 }
 
-//188 was the G value for background; map(second(), 0, 59, 0, 255)
-// text size: map(minute(), 0, 59, 12, 100)
 function draw() {
 	background(143, 188, 143);
 	fill(152, 251, 152);
-	//push();
-	//rotate(map(second(), 0, 23, 0, 2*PI))
 	triangle(45, 45, 45, 360, 200, 360);
-	//pop();
 	arc(479, 300, 280, 280, PI, TWO_PI)
 	textSize(54);
 	textFont('Georgia');
