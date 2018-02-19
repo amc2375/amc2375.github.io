b9d1ffc230670eb96bdca171aed188ab3b7fdfd9
amc2375 <amc2375@barnard.edu>
2018-02-18 23:12:32 -0500

    small edits to text1 and initial draft of interactive

--- A3/interactive.js
+++ A3/interactive.js
@@ -1,5 +1,6 @@
 
-var headlines = [];
+var headlines = [], bylines = [];
+var minlen, maxlen;
 var womenwords = [
 	"woman",
 	"women",
@@ -25,7 +26,7 @@ var trumpwords = [
 	"trump",
 	"trump's",
 	"pence",
-	"pence's",
+	"pence's"
 	];
 
 function preload() {
@@ -45,22 +46,40 @@ function setup() {
 	background(50);
 	textAlign(LEFT);
 	textSize(14);
-	noLoop();
+	//noLoop();
 
 	extractHeadlines();
-//	extractAbstracts();
+	extractBylines();
+
 }
 
 function draw() {
-
+	background(50);
 	var lineheight = 24;
 	var margin = 40;
+	var rectH = 14;
 	translate(margin, margin);
+
+	for (var i = 0; i < headlines.length; i++) {
+		noStroke();
+		var rectW = textWidth(headlines[i]);
+		if (mouseX > margin && 
+			mouseX < margin+rectW && 
+			mouseY < margin+i*lineheight && 
+			mouseY > margin+i*lineheight+(-1*rectH)) {
+			fill(255, 150);
+		}
+		else {
+			fill(50);
+		}
+		rect(0, i*lineheight+3, rectW, -1*rectH);
+	}
 
 	for (var i = 0; i < headlines.length; i++) {
 		var words = split(headlines[i], ' ');
 		var nextX = 0;
 
+		//noStroke();
 		for (var j = 0; j < words.length; j++) {
 			if(womenwords.includes(words[j].toLowerCase())) {
 				fill('pink');
@@ -69,18 +88,16 @@ function draw() {
 			else if (trumpwords.includes(words[j].toLowerCase())) {
 				fill(80);
 				words[j] = words[j].replace("Trump", "T****");
-
 			}
 			else {
-				textSize(14);
+				textSize(14)
 				textStyle(NORMAL);
-				fill(150);
+				fill(100);
 			}
 
 			text(words[j] + ' ', nextX, i*lineheight);
 			nextX += textWidth(words[j] + ' ');
 		}
-
 	} 
 	push();
 	stroke('pink');
@@ -96,6 +113,31 @@ function draw() {
 	textStyle(ITALIC);
 	text("LESS TRUMP", 750, 350);
 	pop();
+
+
+	for (var i = 0; i < headlines.length; i++) {
+		var rectW2 = textWidth(headlines[i]);
+		if (mouseX > margin && 
+			mouseX < margin+rectW2 && 
+			mouseY < margin+i*lineheight && 
+			mouseY > margin+i*lineheight+(-1*rectH)) {
+			fill(255, 192, 203, 100);
+			//var boxW = textWidth(bylines[i]);
+			rect(mouseX, mouseY, 230, 30);
+			fill(255);
+			textSize(20);
+			text("click to see who wrote this", mouseX+10, mouseY+10);
+			if(mouseIsPressed) {
+				push();
+				textSize(50);
+				textAlign(CENTER);
+				text(bylines[i], width/2, 400);
+				pop();
+			}
+			//text(bylines[i], mouseX+10, mouseY+30);
+		}
+	}
+
 }
 
 function extractHeadlines() {
@@ -103,10 +145,33 @@ function extractHeadlines() {
 
 	for (var i = 0; i < nytResponse.results.length; i++) {
 		var h = nytResponse.results[i].title;
+
+		if (!maxlen) {
+			maxlen = h.length;
+		}
+		else if (h.length > maxlen) {
+			maxlen = h.length;
+		}
+
+		if (!minlen) {
+			minlen = h.length;
+		}
+		else if (h.length < minlen) {
+			minlen = h.length
+		}
+
 		append(headlines, h);
 	}
-
 	//console.log(headlines);
 
 } 
+
+function extractBylines() {
+	for (var i = 0; i < nytResponse.results.length; i++) {
+		var b = nytResponse.results[i].byline;
+		b = b.replace("By ", "");
+		b = b.toLowerCase();
+		append(bylines, b);
+	}
+}
 
